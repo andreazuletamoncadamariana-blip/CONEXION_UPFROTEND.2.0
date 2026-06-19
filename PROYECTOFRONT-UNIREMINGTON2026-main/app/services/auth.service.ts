50905862fe9052e45593d6b2a8622d49b5affde1
mariana andrea zuleta moncada <andreazuletamoncadamariana@gmail.com>
2026-06-18 23:06:37 -0500

    mi primer :mi frotend listo

--- PROYECTOFRONT-UNIREMINGTON2026-main/app/services/auth.service.ts
+++ PROYECTOFRONT-UNIREMINGTON2026-main/app/services/auth.service.ts
@@ -1,26 +1,58 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Injectable,signal} from '@angular/core';
+import{ HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environments';
+import {LoginRequest ,Authresponse } from '..core/models/estudiante.model';;
+
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  // Cambia esta URL por la de tu API real
-  private apiUrl = 'https://api.tu-servidor.com/auth';
+export class Auth {
+  private apiurl = '${environment.apiUrl}/auth';
+  
+  currentUserRole =signal<string| null>(this.getStoreRole());
+  isLoggedIn = signal <boolean>(this.hasToken());
+ 
+ constructor(private http: HttpClient){}
 
-  constructor(private http: HttpClient) {}
+ login(credentials: LoginRequest): Observable<Authresponse> {
+    return this.http.post<Authresponse>('${this.apiUrl}/login', credentials)
+    .pipe(
+      tap(response => this.guardarSesion(response))
+    );
 
-  login(credentials: any): Observable<any> {
-    // Simulación de login para pruebas
-    console.log('Intentando login con:', credentials);
-    return of({ success: true, token: 'fake-jwt-token' });
-    // Real: return this.http.post(`${this.apiUrl}/login`, credentials);
-  }
 
-  sendResetPasswordEmail(email: string): Observable<any> {
-    console.log('Enviando recuperación a:', email);
-    return of({ success: true });
-    // Real: return this.http.post(`${this.apiUrl}/forgot-password`, { email });
-  }
-}
+    private guardarSesion(response: Authresponse): void {
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('role',response.role);
+      localStorage.setItem('email', response.email);
+      
+      this.currentUserRole.set(response.email);
+      this.isLoggedIn.set(true);
+    }
+    
+    Logout(): void {
+      localStorage.removeItem('token');
+      localStorage.removeItem('role');
+      localStorage.removeItem('email');
+
+      this.currentUserRole.set('');
+      this.isLoggedIn.set(false);
+    }
+
+ getToken(): string | null {
+      return localStorage.getItem('token');
+ }
+
+ getRole(): string | boolean {
+      return !! localStorage.getItem('role');
+ }
+
+ private hasToken():boolean {
+  return !! localStorage.getItem('token');
+ }
+
+ private getStoreRole():string | null {
+  return localStorage.getItem('role');
